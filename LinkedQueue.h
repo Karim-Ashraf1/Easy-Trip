e2db63db3f1a2271e3d9b74d911d9250ea933275
kimosoft1 <kariiimashraf.1@gmail.com>
2023-12-02 18:16:47 +0200

    modified:   LinkedQueue.h

--- LinkedQueue.h
+++ LinkedQueue.h
@@ -1,22 +1,24 @@
 #ifndef LINKED_QUEUE_
 #define LINKED_QUEUE_
 
-
 #include "Node.h"
 #include "QueueADT.h"
 
 template <typename T>
-class LinkedQueue:public QueueADT<T>
+class LinkedQueue : public QueueADT<T>
 {
-private :
-	Node<T>* backPtr;
-	Node<T>* frontPtr;
-public :
-	LinkedQueue();	
-	bool isEmpty() const ;
-	bool enqueue(const T& newEntry);
-	bool dequeue(T& frntEntry);  
-	bool peek(T& frntEntry)  const;	
+private:
+	Node<T> *backPtr;
+	Node<T> *frontPtr;
+
+public:
+	LinkedQueue();
+	bool isEmpty() const;
+	bool enqueue(const T &newEntry);
+	bool priorityEnqueue(const T &newEntry);
+	int getPriority(const T &entry) const;
+	bool dequeue(T &frntEntry);
+	bool peek(T &frntEntry) const;
 	~LinkedQueue();
 };
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -30,9 +32,8 @@ The constructor of the Queue class.
 template <typename T>
 LinkedQueue<T>::LinkedQueue()
 {
-	backPtr=nullptr;
-	frontPtr=nullptr;
-
+	backPtr = nullptr;
+	frontPtr = nullptr;
 }
 /////////////////////////////////////////////////////////////////////////////////////////
 
@@ -46,7 +47,7 @@ Output: True if the queue is empty; otherwise false.
 template <typename T>
 bool LinkedQueue<T>::isEmpty() const
 {
-	return (frontPtr==nullptr);
+	return (frontPtr == nullptr);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -59,19 +60,95 @@ Output: True if the operation is successful; otherwise false.
 */
 
 template <typename T>
-bool LinkedQueue<T>::enqueue( const T& newEntry)
+bool LinkedQueue<T>::enqueue(const T &newEntry)
 {
-	Node<T>* newNodePtr = new Node<T>(newEntry);
+	Node<T> *newNodePtr = new Node<T>(newEntry);
 	// Insert the new node
-	if (isEmpty())	//special case if this is the first node to insert
+	if (isEmpty())			   // special case if this is the first node to insert
 		frontPtr = newNodePtr; // The queue is empty
 	else
 		backPtr->setNext(newNodePtr); // The queue was not empty
 
 	backPtr = newNodePtr; // New node is the last node now
-	return true ;
+	return true;
 } // end enqueue
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*Function:prioritiy-enqueue
+Adds newEntry in the queue accourding to its priority.
+
+Input: newEntry .
+Output: True if the operation is successful; otherwise false.
+*/
+
+// this code is made by the leader "karim";
+template <typename T>
+bool LinkedQueue<T>::priorityEnqueue(const T &newEntry)
+{
+	Node<T> *newNodePtr = new Node<T>(newEntry);
+
+	int priority = getPriority(newEntry);
+
+	if (isEmpty())
+	{
+		frontPtr = newNodePtr; // kariiiiim
+		backPtr = newNodePtr;
+		return true;
+	}
+
+	if (priority > getPriority(frontPtr->getItem()))
+	{
+		newNodePtr->setNext(frontPtr);
+		frontPtr = newNodePtr;
+	}
+	else
+	{
+		Node<T> *currentPtr = frontPtr;
+		while (currentPtr->getNext() != nullptr && priority <= getPriority(currentPtr->getNext()->getItem()))
+		{
+			currentPtr = currentPtr->getNext(); // leaderrrr
+		}
+
+		newNodePtr->setNext(currentPtr->getNext());
+		currentPtr->setNext(newNodePtr);
+
+		if (newNodePtr->getNext() == nullptr)
+		{
+			backPtr = newNodePtr; // kariiim is the leaderrrr
+		}
+	}
+
+	return true;
+}
+
+/*Function:getPriority
+checks the input priority.
+
+Input: newEntry .
+Output: int shows the priority of the input.
+*/
+
+template <typename T>
+int LinkedQueue<T>::getPriority(const T &entry) const
+{
+	if (entry == "SP")
+	{ // karim is the highest priority of course
+		return 3;
+	}
+	else if (entry == "WP")
+	{ // nour is the secound
+		return 2;
+	}
+	else if (entry == "NP")
+	{ // taha is the third
+		return 1;
+	}
+	else
+	{ // faris is not a priority
+		return 0;
+	}
+}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -84,25 +161,23 @@ Output: True if the operation is successful; otherwise false.
 */
 
 template <typename T>
-bool LinkedQueue<T>:: dequeue(T& frntEntry)  
+bool LinkedQueue<T>::dequeue(T &frntEntry)
 {
-	if(isEmpty())
+	if (isEmpty())
 		return false;
 
-	Node<T>* nodeToDeletePtr = frontPtr;
+	Node<T> *nodeToDeletePtr = frontPtr;
 	frntEntry = frontPtr->getItem();
 	frontPtr = frontPtr->getNext();
 	// Queue is not empty; remove front
-	if (nodeToDeletePtr == backPtr)	 // Special case: last node in the queue
-		backPtr = nullptr ;	
-		
+	if (nodeToDeletePtr == backPtr) // Special case: last node in the queue
+		backPtr = nullptr;
+
 	// Free memory reserved for the dequeued node
 	delete nodeToDeletePtr;
 
 	return true;
 }
-
-
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
@@ -115,31 +190,31 @@ Output: The front of the queue.
 */
 
 template <typename T>
-bool LinkedQueue<T>:: peek(T& frntEntry) const 
+bool LinkedQueue<T>::peek(T &frntEntry) const
 {
-	if(isEmpty())
+	if (isEmpty())
 		return false;
 
 	frntEntry = frontPtr->getItem();
 	return true;
-
 }
 ///////////////////////////////////////////////////////////////////////////////////
 
 template <typename T>
 LinkedQueue<T>::~LinkedQueue()
 {
-	//Note that the cout statements here is just for learning purpose
-	//They should be normally removed from the destructor
-	cout<<"\nStarting LinkedQueue destructor...";
-	cout<<"\nFreeing all nodes in the queue...";
+	// Note that the cout statements here is just for learning purpose
+	// They should be normally removed from the destructor
+	cout << "\nStarting LinkedQueue destructor...";
+	cout << "\nFreeing all nodes in the queue...";
 
-	//Free all nodes in the queue
+	// Free all nodes in the queue
 	T temp;
-	while(dequeue(temp));
-	
-	cout<<"\n Is LinkedQueue Empty now?? ==> "<<boolalpha<<isEmpty();
-	cout<<"\nEnding LinkedQueue destructor..."<<endl;
+	while (dequeue(temp))
+		;
+
+	cout << "\n Is LinkedQueue Empty now?? ==> " << boolalpha << isEmpty();
+	cout << "\nEnding LinkedQueue destructor..." << endl;
 }
 
 #endif
