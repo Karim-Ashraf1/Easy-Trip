0211c0ad6e5df4e58925e0cf022bbb6dcf99f94e
nourwan <nour.awad094@gmail.com>
2023-12-09 00:31:40 +0200

    Bus.h

--- Bus.h
+++ Bus.h
@@ -1,21 +1,27 @@
 #include <iostream>
 using namespace std;
+#include <LinkedQueue.h>
+
 class Bus {
 private:
     int id;
     string type;
-    int capacity;
-    int maintenanceTime;
-    int maintenanceJourneys;
-
+    int capacity = 45;
+    int maintenanceT; // for time
+    int maintenanceJ; // for journeys
+    LinkedQueue WBus; // for wheelchair
+    LinkedQueue MBus; // for normal people
 
 public:
-    Bus(int id, string type, int capacity, int maintenanceTime, int maintenanceJourneys)
-        : id(id), type(type), capacity(capacity), maintenanceTime(maintenanceTime), maintenanceJourneys(maintenanceJourneys) {}
+    Bus(int id, string type, int maintenanceT, int maintenanceJ, LinkedQueue WBus, LinkedQueue MBus)
+        : id(id), type(type), maintenanceT(maintenanceT), maintenanceJ(maintenanceJ), WBus(WBus), MBus(MBus) {}
 
     int getId() { return id; }
     string getType() { return type; }
-    int getCapacity() { return capacity; }
-    int getMaintenanceTime() { return maintenanceTime; }
-    int getMaintenanceJourneys() { return maintenanceJourneys; }
+    int getMaintenanceTime() { return maintenanceT; }
+    int getMaintenanceJourneys() { return maintenanceJ; }
+    LinkedQueue getWBus() { return WBus;}
+    LinkedQueue getMBus() { return MBus;}
+
+
 };
