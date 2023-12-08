50db74e485d2ec9d08b669f5ea4ec15d15672ef5
Ahmed-Taha-981 <122402269+Ahmed-Taha-981@users.noreply.github.com>
2023-12-08 22:44:11 +0200

    modified:   Passenger.h

--- Passenger.h
+++ Passenger.h
@@ -3,7 +3,6 @@
 using namespace std;
 
 class Passenger {
-
 private:
     int id;
     int startStation;
@@ -12,14 +11,18 @@ private:
     string type;
 
 public:
+    // Constructor 
     Passenger(int id, int startStation, int endStation, int getOnOffTime, string type)
         : id(id), startStation(startStation), endStation(endStation), getOnOffTime(getOnOffTime), type(type) {}
 
+    // Getter method for each attribute
     int getId() const { return id; }
     int getStartStation() const { return startStation; }
     int getEndStation() const { return endStation; }
     int getGetOnOffTime() const { return getOnOffTime; }
     string getType() const { return type; }
+
+
 
 
 };
