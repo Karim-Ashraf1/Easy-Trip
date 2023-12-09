bc7328e434046ab08648914fb4a687376c7a1d15
Ahmed-Taha-981 <122402269+Ahmed-Taha-981@users.noreply.github.com>
2023-12-09 15:48:53 +0200

    modified:   Bus.h 	modified:   LinkedList.h 	modified:   Station.h 	modified:   Passenger.h

--- Bus.h
+++ Bus.h
@@ -1,27 +1,22 @@
-<<<<<<< HEAD
-
-=======
 #pragma once
->>>>>>> d9a6a53286efc62ae0faccdee840f0c3fbe99ac0
 #include <iostream>
 #include <LinkedQueue.h>
 #include "Passenger.h"
-
 class Bus {
 private:
     int id;
     std::string type;
-    int capacity = 45;
+    static const int capacity = 45;
     int CurrentLoad;
     int maintenanceT; // for time
-    int maintenanceJ; // for journeys
-    Passenger PassengerInBus[];
+    int maintenanceJ; // for journeys    
 public:
-    Bus(int id, string type, int maintenanceT, int maintenanceJ)
+    Passenger PassengersInBus[capacity];
+    Bus(int id, std::string type, int maintenanceT, int maintenanceJ)
         : id(id), type(type), maintenanceT(maintenanceT), maintenanceJ(maintenanceJ) {}
     //getters for attributes
     int getId() { return id; }
-    string getType() { return type; }
+    std::string getType() { return type; }
     int getMaintenanceTime() { return maintenanceT; }
     int getMaintenanceJourneys() { return maintenanceJ; }
     int getCurrentLoad() {return CurrentLoad ;}
