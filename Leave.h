6c3c5d6a21eddaa6133e4cf4f90e07ecb2825a7f
kimosoft1 <kariiimashraf.1@gmail.com>
2023-12-09 18:26:46 +0200

    modified:   Leave.h

--- Leave.h
+++ Leave.h
@@ -12,16 +12,31 @@ private:
     bool L;        // means this is a LEAVE event line
     int ET;        // event timestep
     int ID;        // id if the passengerswho decided to leave
-    Station STRT;  // start station of the passenger asking to leave
+    int STRT;  // start station of the passenger asking to leave
     Passenger *LP; // LP= Leaving Passenger
     Passenger *DP; // DP= Dequeued Passenger
-protected:
+public:
+
+    // setters for all attrubiutes of Leave
+    void SetID(int id){ID=id;}
+    void SetStartStation(int start){STRT=start;}
+    void setEventTimeStep(int et){ET=et;}
+
+
+    /// getters for all attrubiutes of Leave
+    int GetID() const {return ID;}
+    int GetStartStation()const {return STRT;}
+    int GetEventTimeStep() const {return ET;}
+
+    /// overloaded Excute() in Events class
     void Execute()
     {
         LinkedList<Passenger *> appropiate;  // List of pointers to passengers
         LinkedQueue<Passenger *> leavequeue; // queue that will save passengers in bus until removing the leaving passenger
 
-        DP = appropiate.Find(LP); //
-        leavequeue.dequeue(DP);
+        // DP = appropiate.Find(LP); //
+        // leavequeue.dequeue(DP);
+
+
     }
 };
