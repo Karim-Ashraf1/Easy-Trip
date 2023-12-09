bf1322fc90a745be1cf43cb6e364699040cfb4cc
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2023-12-09 02:00:28 +0200

    Modified Arrive

--- Arrive.h
+++ Arrive.h
@@ -1,14 +1,62 @@
+#pragma once
+#include <Time.h>
 #include<iostream> 
 #include<Station.h>
 #include<Passenger.h>
+#include<LinkedList.h>
 using namespace std;
 
-class Arrive{
+
+class Arrive
+{
 private:
     Passenger Psngr ; // Creating a Passenger instance 
-    int ET ; // Event timestep
+    //Time ET ; // Event timestep          ///to be implemented later
     int ID ; // Unique number that identifies each passenger
-    Station Stn ; // Create a station instance
-protected:
-    void Execute();
+    int StartStation ; // Shows the starting station
+    int EndStation ; // Shows the ending station
+    int OnTime ; // Shows the the time of the passenger getting on th bus
+    int OffTime ; // Shows the time the passenger gets off the bus
+    string Type ; // The type of passenger
+    LinkedList<Passenger*> appropiate ; //list of pointers to passengers 
+
+public:
+
+    /// setters for all attrubiutes of arrive(which will be used to set the attributes of passenger)
+    void SetID(int id){ID=id;}
+    void SetStartStation(int start){StartStation=start;}
+    void SetEndStation(int End){EndStation=End;}
+    void SetOnTime(int on_time){OnTime=on_time;}
+    void SetOffTime(int Off_time){OffTime=Off_time;}
+    void SetType(string type){Type=type;}
+
+
+    /// getters for all attrubiutes of arrive(which are for passenger)
+    int GetID() {return ID;}
+    int GetStartStation() {return StartStation;}
+    int GetEndStation() {return EndStation;}
+    int GetOnTime() {return OnTime;}
+    int GetOffTime() {return OffTime;}
+    string GetType() {return Type;}
+    
+
+
+    /// overloaded Excute() in Events class
+    void Execute()
+    {
+
+        /// Takes Attrubiutes of Arrive and sets the passenger instance by them 
+        Psngr.setId(ID);
+        Psngr.setStartStation(StartStation);
+        Psngr.setEndStation(EndStation);
+        Psngr.setGetOFFTime(OffTime);
+        Psngr.setGetOnTime(OnTime);
+        Psngr.setType(Type);
+
+        Passenger *psgr_ptr=&Psngr;
+
+        appropiate.InsertEnd(psgr_ptr); /// adding The passenger to linked list
+    }
+    void SetET(int et);  ///to be implemented later
+    int GetET();  ///to be implemented later
 };
