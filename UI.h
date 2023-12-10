760c81bd657ce1e2957c09a15b891e6d43c87f60
kimosoft1 <kariiimashraf.1@gmail.com>
2023-12-10 19:55:11 +0200

    modified:   UI.h

---
+++ UI.h
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<Passenger.h>
+#include<Bus.h>
+#include<Station.h>
+#include<Events.h>
+
+class UI {
+private:
+
+public:
+    void PrintOutput(){
+        // print the final output
+    }
+};
