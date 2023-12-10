3284e1c68150c2b04a0b330237e2c86b7eba1b98
kimosoft1 <kariiimashraf.1@gmail.com>
2023-12-10 23:11:21 +0200

    modified:   Time.h

--- Time.h
+++ Time.h
@@ -1,24 +1,31 @@
 #include <iostream>
-#include <iomanip>
-#include <thread>  // for std::this_thread::sleep_for
-#include <chrono>  // for std::chrono::seconds
 
-struct Time {
+class Time {
+private:
     int hours;
     int minutes;
     int seconds;
+
+public:
+    Time(int h, int m, int s)
+    {
+        int hours = h;
+        int minutes = m;
+        int secound = s;
+    }
+
+    Time simulateTimePassage(const Time &currentTime, int secondsPassed)
+    {
+        Time newTime = currentTime;
+
+        // Convert everything to seconds and add the simulated time passage
+        int totalSeconds = currentTime.hours * 3600 + currentTime.minutes * 60 + currentTime.seconds + secondsPassed;
+
+        // Calculate new hours, minutes, and seconds
+        newTime.hours = totalSeconds / 3600 % 24;
+        newTime.minutes = (totalSeconds % 3600) / 60;
+        newTime.seconds = totalSeconds % 60;
+
+        return newTime;
+    }
 };
-
-Time simulateTimePassage(const Time& currentTime, int secondsPassed) {
-    Time newTime = currentTime;
-
-    // Convert everything to seconds and add the simulated time passage
-    int totalSeconds = currentTime.hours * 3600 + currentTime.minutes * 60 + currentTime.seconds + secondsPassed;
-
-    // Calculate new hours, minutes, and seconds
-    newTime.hours = totalSeconds / 3600 % 24;
-    newTime.minutes = (totalSeconds % 3600) / 60;
-    newTime.seconds = totalSeconds % 60;
-
-    return newTime;
-}
