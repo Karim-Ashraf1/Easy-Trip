2d563ed8cab91bddc4bb47262bd95e805c789dd7
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2023-12-11 23:16:48 +0200

    Edited StartProgram

--- Company.h
+++ Company.h
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include "Events.h"
+#include "Leave.h"
 #include "Bus.h"
 #include "Station.h"
 #include "LinkedQueue.h"
@@ -16,8 +17,14 @@ private:
 
 public:
 
-void StartProgram(ifstream& fileName,string ArrayOut[])
+int StartProgram(ifstream& fileName)
 {
+    int FileArray[9];
+    Arrive arival;
+    Leave_Event Leaver;
+    string ArrayArrive[9];
+    string ArrayLeave[3];
+    string ArrayOut[9];
     fstream ReadFile;
     string word, t, q, filename;
 
@@ -27,14 +34,25 @@ void StartProgram(ifstream& fileName,string ArrayOut[])
     // opening file
     ReadFile.open(filename.c_str());
 
+    
+
     // extracting words from the file
 	int i=0;
     while (ReadFile >> word)
-    {	
-		ArrayOut[i]=word;
+    {
+    	ArrayOut[i]=word;
         // displaying content
 		i++;
     }
+
+    if (ArrayOut[0] == "A")
+    {
+        arival.Execute(FileArray);
+    }
+    if (ArrayOut[0] == "L"){
+        Leaver.Execute(FileArray);
+    }
+
 	ReadFile.close();
 }
 
