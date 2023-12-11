75a74e659cd5b7cb8814a3ba20ee813cae14d5bf
kimosoft1 <kariiimashraf.1@gmail.com>
2023-12-11 22:55:32 +0200

    modified:   Events.h

--- Events.h
+++ Events.h
@@ -5,7 +5,7 @@ class Events{
 private:
     
 public:
-    virtual void Execute() =0 {
+    virtual void Execute(int FileArray[], int Size) =0 {
         //Pure virtual function to be executed by inherited classes
     }
 };
