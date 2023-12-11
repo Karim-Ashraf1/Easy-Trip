6de96d40ea82ffc276ab61521ed46034170fa7da
kimosoft1 <kariiimashraf.1@gmail.com>
2023-12-11 19:38:53 +0200

    modified:   Company.h

--- Company.h
+++ Company.h
@@ -4,6 +4,8 @@
 #include <Station.h>
 #include<DoubleLinkedList.h>
 #include <UI.h>
+#include<fstream>
+#include <sstream>
 using namespace std;
 
 class Company {
@@ -17,6 +19,34 @@ private:
 public:
     Company(string& inputFile, string& outputFile)
         : InputFile(inputFile), OutputFile(outputFile) {}
+
+
+    void ReadFile(const string& filename, vector<vector<string>>& InputList) {
+        // Open the file
+        ifstream file(filename);
+        if (!file.is_open()) {
+            cerr << "Error opening file: " << filename << endl;
+            return;
+        }
+
+        // Read lines from the file
+        string line;
+        while (getline(file, line)) {
+            // Split the line into words
+            istringstream iss(line);
+            vector<string> words;
+            string word;
+            while (iss >> word) {
+                words.push_back(word);
+            }
+
+            // Add the vector of words to InputList
+            InputList.push_back(words);
+        }
+
+        // Close the file
+        file.close();
+    }
 
     void StartProgram() {
         // read the file
