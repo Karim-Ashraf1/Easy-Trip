3a8e0c9460f2612af3abea6790edfacacd4329db
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2023-12-11 22:32:58 +0200

    same as last push

--- Company.h
+++ Company.h
@@ -15,44 +15,8 @@ private:
     DoubleLinkedList <Station*> StationsList;
 
 public:
-    void ReadFile(const string& filename, vector<vector<string>>& InputList) {
-        // Open the file
-        ifstream file(filename);
-        if (!file.is_open()) {
-            cerr << "Error opening file: " << filename << endl;
-            return;
-        }
 
-        // Read lines from the file
-        string line;
-        while (getline(file, line)) {
-            // Split the line into words
-            istringstream iss(line);
-            vector<string> words;
-            string word;
-            while (iss >> word) {
-                words.push_back(word);
-            }
-
-            // Add the vector of words to InputList
-            InputList.push_back(words);
-        }
-
-        // Close the file
-        file.close();
-
-        // print result
-        for (const auto& line : InputList) {
-        for (const auto& word : line) {
-            cout << word << " ";
-            }
-        cout << endl;
-        }
-    }
-
-    void StartProgram() {
-        // read the file
-       void StartProgram(ifstream& fileName,string ArrayOut[])
+void StartProgram(ifstream& fileName,string ArrayOut[])
 {
     fstream ReadFile;
     string word, t, q, filename;
@@ -77,7 +41,6 @@ public:
         // add events to EventsList
 
         // add buses to BusesList
-    }
 
     void simulateProgram() {
         // Execute the events that should be executed at that min
