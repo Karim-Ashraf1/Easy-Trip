4e3f649701e4bd7557298b8e3507eb0852edfaf3
Ahmed-Taha-981 <122402269+Ahmed-Taha-981@users.noreply.github.com>
2023-12-11 14:06:06 +0200

    modified:   Station.h

--- Station.h
+++ Station.h
@@ -34,19 +34,19 @@ public:
         return AvailableBuses.dequeue(bus);
     }
 
-    void checkEndStationAndRemove(Station x, Bus busx)
+    void checkEndStationAndRemove(Station stationx, Bus busx)
     {
         int i=0;
       while ( i < 45)
        {
             //create a pointer that points to the same passenger in the queue 
-            Passenger* C ;
-            C=busx.PassengersInBus[i];
+            Passenger* pntr ;
+            pntr=busx.PassengersInBus[i];
             //create a passenger that shares the same attributes as the passenger that the pointer points to
-            Passenger p;
-            p=*C;
+            Passenger psngr;
+            psngr=*pntr;
             // Check if the end station of the passenger is the current station
-            if (p.getEndStation() == x.getNumber()) 
+            if (psngr.getEndStation() == stationx.getNumber()) 
             {
                 //option to announce arrival
                 // std::cout << "Passenger " << passengers[i].getId() << " dequeued at station " << number << std::endl;
@@ -71,17 +71,15 @@ public:
                      for (int i = 0; i < 45; ++i) 
                     {
                         //create a pointer that points to the same passenger in the queue 
-                            Passenger* C ;
-                             C=bus.PassengersInBus[i];
+                            Passenger* pntr ;
+                             pntr=bus.PassengersInBus[i];
                         //create a passenger that shares the same attributes as the passenger that the pointer points to
-                            Passenger p;
-                             p=*C;
-                         if (p.getId()==0) 
+                            Passenger psngr;
+                             psngr=*pntr;
+                         if (psngr.getId()==0) 
                         {
-                            Passenger* P;
-                            P=&p;
                                  // Add the passenger pointer to the array at the first available index
-                            bus.PassengersInBus[i] = P;
+                            bus.PassengersInBus[i] = pntr;
                             bus.setCurrentLoad((bus.getCurrentLoad()+1));
                             bus.setTotalPassengers((bus.getTotalPassengers()+1));
                         }    
