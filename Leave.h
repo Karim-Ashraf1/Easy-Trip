820fa0a947b27f2602070a2ea64dd2d92e6619c2
kimosoft1 <kariiimashraf.1@gmail.com>
2023-12-11 20:57:14 +0200

    modified:   Leave.h

--- Leave.h
+++ Leave.h
@@ -15,19 +15,21 @@ private:
     int ID;        // id if the passengerswho decided to leave
     int STRT;      // start station of the passenger asking to leave
     Passenger *LP; // LP= Leaving Passenger
-    Passenger *DP; // DP= Dequeued Passenger
+    //Passenger *DP; // DP= Dequeued Passenger
 public:
     // setters for all attrubiutes of Leave
     void SetID(int id) { ID = id; }
     void SetStartStation(int start) { STRT = start; }
     void setEventTimeStep(int et) { ET = et; }
     void setL(int l) { L = l; }
+    void set_LP(Passenger* p){LP=p;}
 
     /// getters for all attrubiutes of Leave
     int GetID() const { return ID; }
     int GetStartStation() const { return STRT; }
     int GetEventTimeStep() const { return ET; }
-    int GrtLeaveEvent() const { return L; }
+    int GetLeaveEvent() const { return L; }
+    Passenger* Get_LP() const {return LP;}
 
     /// overloaded Excute() in Events class
     void Execute()
