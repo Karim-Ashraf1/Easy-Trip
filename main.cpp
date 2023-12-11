124a703c924fd4bcb12b63a25d3cb0ae87f8518c
nourwan <nour.awad094@gmail.com>
2023-12-11 23:22:16 +0200

    hagsg

--- main.cpp
+++ main.cpp
@@ -4,37 +4,29 @@ using namespace std;
 #include "Company.h"
 #include <fstream>
 
+int main()
+{
 
-int main(){
+    Station x(1, 5);
+    Time y(4, 23, 5);
+    Time v(9, 8, 6);
+    Passenger psngr1(1, 1, 2, y, v, "NP");
+    x.AddToFinishList(psngr1);
+    x.printFinishListAttributes();
 
-Station x(1,5); 
-Time y (4,23,5);
-Time v (9,8,6);
-Passenger psngr1(1,1,2,y,v,"NP");
-x.AddToFinishList(psngr1);
-x.printFinishListAttributes();
+    // Create a text file
+    ofstream WriteFile("filename.txt");
 
+    // Write to the file
+    WriteFile << "Files can be tricky, but it is fun enough!";
 
+    // Close the file
+    WriteFile.close();
 
+    Company cmp;
+    string filename[20];
+    ifstream readfile("files.txt");
+    cmp.StartProgram(readfile, filename);
 
-
-
-// Create a text file
-ofstream WriteFile("filename.txt");
-
-// Write to the file
-WriteFile << "A SP 4:19 6 2 1 Pregnant \n"
-              << "A NP 4:31 7 2 1 \n "
-              << "A NP 4:39 8 2 1 \n"
-              << "A SP 4 : 43 9 1 2 Pregnant \n"
-              << "A WP 4 : 51 10 2 1 \n";
-
- // Close the file
-WriteFile.close();
-
-Company cmp;
-ifstream readfile("files.txt");
-cmp.StartProgram(readfile);
-
-return 0;
+    return 0;
 }
