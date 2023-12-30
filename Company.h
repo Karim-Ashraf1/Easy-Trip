b2c80293a4d5b65698d50422275b7a9f1f4fcafa
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2023-12-30 02:04:55 +0200

    Finalized Enqueu Busses To Garage

--- Company.h
+++ Company.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -17,144 +18,155 @@ private:
     LinkedQueue<Bus*> GarageQueue;
 public:
 
-// conversion from string to integer 
-int* ConvertToInt(string Array[], int size)
-{   
-    int Arr[2];
-    for (int i = 0;i < size;i++) {
-        Arr[i] = stoi(Array[i]);
+    // conversion from string to integer 
+    int* ConvertToInt(string Array[], int size)
+    {   
+        int Arr[2];
+        for (int i = 0;i < size;i++) {
+            Arr[i] = stoi(Array[i]);
+        }
+        return Arr;
     }
-    return Arr;
-}
 
-// Takes string and slices it by Space and returns an array converted to integer 
-int* StoreBusToGarage(string str)
-{
-    string word = "";
-    string sentence[2];
-    int count = 0;
-    for (auto x : str)
+    // Takes string and slices it by Space and returns an array converted to integer 
+    int* StoreBusToGarage(string str)
     {
-        if (x == ' ')
+        string word = "";
+        string sentence[2];
+        int count = 0;
+        for (auto x : str)
         {
-            sentence[count] = word;
-            word = "";
-            count++;
+            if (x == ' ')
+            {
+                sentence[count] = word;
+                word = "";
+                count++;
+            }
+            else {
+                word = word + x;
+            }
         }
-        else {
-            word = word + x;
+        sentence[count] = word;
+        int* FinalArrayPtr=ConvertToInt(sentence, 2);
+        return FinalArrayPtr;
+    }
+
+
+
+
+    // function to Enque busess to a queue according to their number from the inout file
+    LinkedQueue<Bus*> EnqueueGarage(const string& fileName)
+    {
+        ifstream file;
+        string filename="file.txt";
+        int current_line=0;
+        string line;
+        LinkedQueue<Bus*> BusQueue;
+        LinkedList<Bus*> WBusList;
+        LinkedList<Bus*> MBusList;
+        int* BussesNumber;
+        int counter=0;
+
+        file.open(filename);
+        if (file.fail())
+        {
+            cout <<"Failed";
+            return;
         }
+        while(!file.eof())
+        {
+            current_line++;
+            getline(file,line);
+            if (current_line == 2)break;
+        }
+    BussesNumber= StoreBusToGarage(line);
+    //  functions to create objects according to number of busses available
+        for (int i=0; i < *(BussesNumber); i++)
+        {
+            Bus *MixedBus = new Bus ();
+            MixedBus->setdirection('F');
+            MixedBus->setId(i);
+            MixedBus->setType("MB");
+            WBusList.InsertEnd(MixedBus);
+        }
+
+        for (int i=0; i < *(BussesNumber+1); i++)
+        {
+            Bus *WheelChairBus = new Bus ();
+            WheelChairBus->setdirection('F');
+            WheelChairBus->setId(i);
+            WheelChairBus->setType("WB");
+            WBusList.InsertEnd(WheelChairBus);
+        }
+
+        bool W=false;
+        bool M=false;
+        do
+        {   
+            if(MBusList.FindByIndex(counter))
+            {
+                M=true;
+            }
+            if(WBusList.FindByIndex(counter))
+            {
+                W=true;
+            }
+            if (W && M)
+            {
+                BusQueue.enqueue(WBusList.FindByIndex(counter));
+                BusQueue.enqueue(MBusList.FindByIndex(counter));
+                counter ++;
+            }
+            if (W && !MBusList.FindByIndex(counter))
+            {
+                M=false;
+                BusQueue.enqueue(WBusList.FindByIndex(counter));
+                counter ++;
+            }
+            if (M && !WBusList.FindByIndex(counter))
+            {
+                W=false;
+                BusQueue.enqueue(MBusList.FindByIndex(counter));
+                counter ++;
+            }
+
+        }while(W || M);
+    //    NpBusQueue.enqueue(BussesNumber+1);// to be solved later
+        
     }
-    sentence[count] = word;
-    int* FinalArrayPtr=ConvertToInt(sentence, 2);
-    return FinalArrayPtr;
-}
+        Arrive arival;
+        Leave_Event Leaver;
+        string ArrayArrive[6];
+        string ArrayLeave[3];
+        string ArrayOut[6];
+        ifstream ReadFile;
+        string word, t, q, filename;
 
+        // filename of the file
+        filename = "filename.txt";
 
+        // opening file
+        ReadFile.open(filename.c_str());
 
+        
 
-// function to Enque busess to a queue according to their number from the inout file
-LinkedQueue<Bus*> EnqueueGarage(const string& fileName)
-{
-    ifstream file;
-    string filename="file.txt";
-    int current_line=0;
-    string line;
-    LinkedQueue<Bus*> BusQueue;
-    LinkedList<Bus*> WBusList;
-    LinkedList<Bus*> MBusList;
-    int* BussesNumber;
+        // extracting words from the file
+        int i=0;
+        while (ReadFile >> word)
+        {
+            ArrayOut[i]=word;
+            // displaying content
+            i++;
+        }
 
-    file.open(filename);
-    if (file.fail()){
-        cout <<"Failed";
-        return;
-    }
-    while(!file.eof()){
-        current_line++;
-        getline(file,line);
-        if (current_line == 2)break;
-    }
-   BussesNumber= StoreBusToGarage(line);
-//  functions to create objects according to number of busses available
-    for (int i=0; i < *(BussesNumber); i++)
-    {
-        Bus *MixedBus = new Bus ();
-        MixedBus->setdirection('F');
-        MixedBus->setId(i);
-        MixedBus->setType("MB");
-        WBusList.InsertEnd(MixedBus);
-    }
+        if (ArrayOut[0] == "A")
+        {
+            arival.Execute(FileArray);
+        }
+        if (ArrayOut[0] == "L"){
+            Leaver.Execute(FileArray);
+        }
 
-    for (int i=0; i < *(BussesNumber+1); i++)
-    {
-        Bus *WheelChairBus = new Bus ();
-        WheelChairBus->setdirection('F');
-        WheelChairBus->setId(i);
-        WheelChairBus->setType("WB");
-        WBusList.InsertEnd(WheelChairBus);
-    }
-//    WpBusQueue.enqueue(BussesNumber);// to be solved later
-//    NpBusQueue.enqueue(BussesNumber+1);// to be solved later
-     
-}
-
-
-void StartProgram(const string& fileName)
-{
-    int FileArray[6];
-    Arrive arival;
-    Leave_Event Leaver;
-    string ArrayArrive[6];
-    string ArrayLeave[3];
-    string ArrayOut[6];
-    ifstream ReadFile;
-    string word, t, q, filename;
-
-    // filename of the file
-    filename = "filename.txt";
-
-    // opening file
-    ReadFile.open(filename.c_str());
-
-    
-
-    // extracting words from the file
-	int i=0;
-    while (ReadFile >> word)
-    {
-    	ArrayOut[i]=word;
-        // displaying content
-		i++;
-    }
-
-    if (ArrayOut[0] == "A")
-    {
-        arival.Execute(FileArray);
-    }
-    if (ArrayOut[0] == "L"){
-        Leaver.Execute(FileArray);
-    }
-
-	ReadFile.close();
-}
-
-        // add events to EventsList
-
-        // add buses to BusesList
-
-    void simulateProgram() {
-        // Execute the events that should be executed at that min
-
-        // Move buses from available to moving to checkup to available again
-
-        // Collect statistics that are needed to create output file
-
-        // Calls UI class functions to print details on the output screen
-
-    }
-
-    void produceOutputFile() {
-        // Generate the output file at the end of the simulation
-    }
-};
+        ReadFile.close();
+    };
+        
