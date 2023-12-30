b908df6b5e82cb208b1fff5690511619184c93b5
kimosoft1 <kariiimashraf.1@gmail.com>
2023-12-30 09:30:55 +0200

    modified:   Passenger.h

--- Passenger.h
+++ Passenger.h
@@ -8,8 +8,8 @@ private:
     int id;
     int startStation;
     int endStation;
-    int OnTime;
-    int OFFTime;
+    Time OnTime;
+    Time OFFTime;
     std::string type;
     std::string subtype;
 
@@ -17,23 +17,23 @@ public:
     // Constructors
     Passenger() : id(0), startStation(0), endStation(0), OnTime(), OFFTime(), type(" ") {}
 
-    Passenger(int idx, int startStationx, int endStationx, int OnTimex, int OFFTimex, const std::string& typex)
+    Passenger(int idx, int startStationx, int endStationx, Time OnTimex, Time OFFTimex, const std::string& typex)
         : id(idx), startStation(startStationx), endStation(endStationx), OnTime(OnTimex), OFFTime(OFFTimex), type(typex) {}
 
     // Getter method for each attribute
     int getId() const { return id; }
     int getStartStation() const { return startStation; }
     int getEndStation() const { return endStation; }
-    int getOFFTime() const { return OFFTime; }
-    int getOnTime() const { return OnTime; }
+    Time getOFFTime() const { return OFFTime; }
+    Time getOnTime() const { return OnTime; }
     std::string getType() const { return type; }
     std::string getsubtype() const { return subtype; }
     // Setter method for each attribute
     void setId(int idx) { id = idx; }
     void setStartStation(int startStationx) { startStation = startStationx; }
     void setEndStation(int endStationx) { endStation = endStationx; }
-    void setOnTime(int getOnTimex) { OnTime = getOnTimex; }
-    void setOFFTime(int getOFFTimex) { OFFTime = getOFFTimex; }
+    void setOnTime(Time getOnTimex) { OnTime = getOnTimex; }
+    void setOFFTime(Time getOFFTimex) { OFFTime = getOFFTimex; }
     void setType(const std::string& typex) { type = typex; }
     void setsubtype(const std::string& subtypex) { subtype = subtypex; }
 };
