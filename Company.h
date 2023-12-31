10328339a225f19d5eebf8f909238a6935fba047
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2023-12-31 22:46:37 +0200

    isoalted functions

--- Company.h
+++ Company.h
@@ -9,6 +9,7 @@
 #include "LinkedQueue.h"
 #include "DoubleLinkedList.h"
 #include "UI.h"
+#include "Function.h"
 using namespace std;
 
 class Company {
@@ -18,66 +19,15 @@ private:
     LinkedQueue<Bus*> GarageQueue;
 public:
 
-    // conversion from string to integer 
-    int* ConvertToInt(string Array[], int size)
-    {   
-        int Arr[2];
-        for (int i = 0;i < size;i++) {
-            Arr[i] = stoi(Array[i]);
-        }
-        return Arr;
-    }
-
-    // Takes string and slices it by Space and returns an array converted to integer 
-    int* StoreBusToGarage(string str)
-    {
-        string word = "";
-        string sentence[2];
-        int count = 0;
-        for (auto x : str)
-        {
-            if (x == ' ')
-            {
-                sentence[count] = word;
-                word = "";
-                count++;
-            }
-            else {
-                word = word + x;
-            }
-        }
-        sentence[count] = word;
-        int* FinalArrayPtr=ConvertToInt(sentence, 2);
-        return FinalArrayPtr;
-    }
-
-
-
 
     // function to Enque busess to a queue according to their number from the inout file
     LinkedQueue<Bus*> EnqueueGarage(const string& fileName)
     {
-        ifstream file;
-        string filename="file.txt";
-        int current_line=0;
-        string line;
         LinkedQueue<Bus*> BusQueue;
         int* BussesNumber;
-        int counter=0;
 
-        file.open(filename);
-        if (file.fail())
-        {
-            cout <<"Failed";
-            return;
-        }
-        while(!file.eof())
-        {
-            current_line++;
-            getline(file,line);
-            if (current_line == 2)break;
-        }
-    BussesNumber= StoreBusToGarage(line);
+        BussesNumber=ConvertToInt(GetFileLine(fileName,2,'O'));
+
     //  functions to create objects according to number of busses available
         for (int i=0; i < *(BussesNumber); i++)
         {
@@ -97,45 +47,24 @@ public:
             BusQueue.enqueue(WheelChairBus);
         }
         return BusQueue;
-        
     }
 
 
     // to be modified later
-    int StartProgram(ifstream& fileName){
-        Arrive arival;
-        Leave_Event Leave;
-        string ArrayArrive[6];
-        string ArrayLeave[3];
-        string ArrayOut[6];
-        ifstream ReadFile;
-        string word, t, q, filename;
+    int StartProgram(string fileName){
+        
+        
+        string* ArrayPtr=GetFileLine(fileName,7,'R');
+
         int FileArray[10]; //to be modified into linked list
 
-        // filename of the file
-        filename = "filename.txt";
 
-        // opening file
-        ReadFile.open(filename.c_str());
-
-        
-
-        // extracting words from the file
-        int i=0;
-        while (ReadFile >> word)
+        if (*ArrayPtr == "A")
         {
-            ArrayOut[i]=word;
-            i++;
+            //arival.Execute(ArrayPtr); // to be fixed when changed to linked list
         }
-
-        if (ArrayOut[0] == "A")
-        {
-            arival.Execute(FileArray);
+        if (*ArrayPtr == "L"){
+            //Leave.Execute(ArrayPtr); // to be fixed when changed to linked list
         }
-        if (ArrayOut[0] == "L"){
-            Leave.Execute(FileArray);
-        }
-
-        ReadFile.close();
     }
 };
