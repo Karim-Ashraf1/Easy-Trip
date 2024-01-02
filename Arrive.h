4a174b2649c449e5f8b77e4ffa501c46ffbd413e
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2024-01-02 21:04:14 +0200

    Deleted unnecesary setters and getters

--- Arrive.h
+++ Arrive.h
@@ -10,36 +10,11 @@ class Arrive : public Events
 {
     private:
         Passenger Psngr;// Creating a Passenger instance
-        Time On;// Setting the on time for the passenger
-      //  Time Off;// Setting the off time for the passenger
-        int ID;// Unique number that identifies each passenger
-        int StartStation;// Shows the starting station
-        bool A;// true if an arrival has been activated
-        int EndStation;//Shows the ending station
-        int OnTime;// Shows the time of the passenger getting on th bus
-        int OffTime;// Shows the time the passenger gets off the bus
-        string Type;// The type of passenger
         PriorityQueue<Passenger *> PassWait; // list of pointers to passengers
 
     public:
-        /// setters for all attrubiutes of arrive(which will be used to set the attributes of passenger)
-        void SetID(int id) { ID = id; }
-        void SetStartStation(int start) { StartStation = start; }
-        void SetEndStation(int End) { EndStation = End; }
-        void SetOnTime(int on_time) { OnTime = on_time; }
-       // void SetOffTime(int Off_time) { OffTime = Off_time; }
-        void SetType(string type) { Type = type; }
-
-        /// getters for all attrubiutes of arrive(which are for passenger)
-        int GetID() const { return ID; }
-        int GetStartStation() const { return StartStation; }
-        int GetEndStation() const { return EndStation; }
-        int GetOnTime() const { return OnTime; }
-      //  int GetOffTime() const { return OffTime; }
-        string GetType() const { return Type; }
-
         /// overloaded Excute() in Events class
-        void Execute(string* FileArray/*to be changed to linked list*/)
+        void Execute(string* FileArray)
         {
           /// Takes Attrubiutes of Arrive and sets the passenger instance by them
           Psngr.setOnTime(*FileArray);         
