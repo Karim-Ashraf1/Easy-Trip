950c24b8d54c22d8427b84736766c7364a5e6800
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2024-01-05 23:05:50 +0200

    Created And Finalized UI

--- UI.h
+++ UI.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include <Passenger.h>
 #include <Bus.h>
@@ -10,27 +11,41 @@
 class UI
 {
 private:
-    int count_Sp;
-    int count_np;
-    int count_wp;
+    bool IsSilent;
+
 public:
-    // setters
-    void set_count_sp(int sp){count_Sp=sp;}
-    void set_count_np(int np){count_np=np;}
-    void set_count_wp(int wp){count_wp=wp;}
-
-    // getters
-    int get_count_sp(){return count_Sp;}
-    int get_count_np(){return count_np;}
-    int get_count_wp(){return count_wp;}
-    
-    
-    void PrintOutput()
-    {
-
+    UI(){
+        IsSilent = true;
     }
-    void waitingpassengers()
-    {
-        Arrive;
+    void Mode(){
+        cout << "Choose mode :" << endl << "1 - Interactive :" << endl << "2 - Silent :" << endl;
+        int Mode;
+        cin >> Mode;
+        if (Mode == 1){
+            IsSilent = false;
+        }
+    }
+    void PrintSimulation(int time , Station* StationsArray,int numberOfStations,LinkedQueue<Passenger*>& FinishedPassenger,LinkedQueue<Bus*>CheckupWBus,LinkedQueue<Bus*>CheckupMbus,LinkedQueue<Bus*>MovingBus ){
+        if(IsSilent){
+            return;
+        }
+        cout << "Time =  "<< time/60 << ":" << time%60 <<endl;
+        for (Bus* bus : CheckupMbus){
+            cout << bus->getId()<< ", ";
+        }
+        for (Bus* bus : CheckupWBus){
+            cout << bus->getId()<<", ";
+        }
+        cout <<"\n *******************************"<<endl;
+        cout<<FinishedPassenger.size()<<"Finished Passengers : ";
+        for (auto Passenger : FinishedPassenger){
+            cout<<Passenger->getId()<<", ";
+        }
+        cout<<endl;
+        system("pause");
+    }
+
+    void EndMessage(){
+        cout<<"Simulation Ended"<<endl<<"Generated Output File";
     }
 };
