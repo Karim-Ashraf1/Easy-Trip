71281615db906b2ae4f9563ab953c7f405480243
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2024-01-05 19:17:12 +0200

    Added Priority to constrctor

--- Arrive.h
+++ Arrive.h
@@ -8,17 +8,18 @@ class Arrive : public Events
         PriorityQueue<Passenger *> PassWait; // list of pointers to passengers
         string PassenegerType;
         int startStation;
+        int priority;
         int endStation;
 
     public:
-     Arrive(int Time, int Id, string PassenegerType, int startStation, int endStation)
+     Arrive(int Time, int Id, string PassenegerType, int startStation, int endStation,int priority=-1)
         :Events(Time, Id),
-        PassenegerType(PassenegerType), startStation(startStation - 1), endStation(endStation - 1) {}
+        PassenegerType(PassenegerType), startStation(startStation - 1), endStation(endStation - 1), priority(priority) {}
         /// overloaded Excute() in Events class
         void Execute(Station *stationsArray)
         {
           /// Takes Attrubiutes of Arrive and sets the passenger instance by them
-          Passenger* Psngr=new Passenger(Id,Time,PassenegerType,startStation, endStation);
+          Passenger* Psngr=new Passenger(Id,Time,PassenegerType,startStation, endStation,priority);
           
           stationsArray[startStation].addPassenger(Psngr);
            /// adding The passenger to linked list
