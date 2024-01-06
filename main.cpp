a81dc71a402370d5db5b5b10e5e4c866d26220cd
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2024-01-06 17:28:55 +0200

    Created Main

--- main.cpp
+++ main.cpp
@@ -1,32 +1,15 @@
 #include <iostream>
 using namespace std;
-#include "Station.h"
 #include "Company.h"
-#include <fstream>
 
 int main()
 {
-
-    Station x(1, 5);
-    Time y(4, 23, 5);
-    Time v(9, 8, 6);
-    Passenger psngr1(1, 1, 2, y, v, "NP");
-    x.AddToFinishList(psngr1);
-    x.printFinishListAttributes();
-
-    // Create a text file
-    ofstream WriteFile("filename.txt");
-
-    // Write to the file
-    WriteFile << "Files can be tricky, but it is fun enough!";
-
-    // Close the file
-    WriteFile.close();
-
-    Company cmp;
-    string filename[20];
-    ifstream readfile("files.txt");
-    cmp.StartProgram(readfile, filename);
-
+    Company* MyCompany = new Company;
+    cout << "Enter The File Name" << endl;
+    string filename;
+    cin >> filename;
+    // MyCompany->ReadFile(filename);
+    MyCompany->Simulate(filename);
+   
     return 0;
 }
