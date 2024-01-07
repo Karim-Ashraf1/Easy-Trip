f24c4c2cb82f2f6eba15c3e6bf5d29efb8b97bba
nourwan <nour.awad094@gmail.com>
2024-01-07 05:20:55 +0200

    add to moving list function

--- Company.h
+++ Company.h
@@ -47,46 +47,51 @@ private:
         // create objects according to number of busses available
         for (int i = 0; i < MbusCount; i++)
         {
-            Bus *MixedBus = new Bus("MBus",MixedBusCapacity,JourneysToCheckup);
+            Bus *MixedBus = new Bus("MBus", MixedBusCapacity, JourneysToCheckup);
             GarageQueue.enqueue(MixedBus);
         }
 
         for (int i = 0; i < WbusCount; i++)
         {
-           Bus *MixedBus = new Bus("WBus",WheelBusCapacity,JourneysToCheckup);
+            Bus *MixedBus = new Bus("WBus", WheelBusCapacity, JourneysToCheckup);
             GarageQueue.enqueue(MixedBus);
         }
     }
 
-    Events* CreateArrive(ifstream& file){
+    Events *CreateArrive(ifstream &file)
+    {
         string PassengerType;
-        int hour, minute,id,startStation,EndStation,Priority=-1;
+        int hour, minute, id, startStation, EndStation, Priority = -1;
         char colon;
-        file>>PassengerType>>hour>>colon>>minute>>id>>startStation>>EndStation;
-        if (PassengerType=="SP"){
+        file >> PassengerType >> hour >> colon >> minute >> id >> startStation >> EndStation;
+        if (PassengerType == "SP")
+        {
             string subType;
-            file>>subType;
-            if(subType== "aged"){
-                Priority=2;
-            }
-            else if(subType=="POD"){
-                Priority=1;
-            }
-            else 
+            file >> subType;
+            if (subType == "aged")
             {
-                Priority=0;
+                Priority = 2;
+            }
+            else if (subType == "POD")
+            {
+                Priority = 1;
+            }
+            else
+            {
+                Priority = 0;
             }
         }
-        int time = hour * 60 +minute;
-        return new Arrive(time,id,PassengerType,startStation,EndStation,Priority);
+        int time = hour * 60 + minute;
+        return new Arrive(time, id, PassengerType, startStation, EndStation, Priority);
     }
 
-    Events* CreateLeave(ifstream& file){
-        int hour,min,id;
+    Events *CreateLeave(ifstream &file)
+    {
+        int hour, min, id;
         char colon;
-        file>>hour>>colon>>min>>id;
-        int time =hour *60 +min;
-        return new Leave(numberOfStations,id,time);
+        file >> hour >> colon >> min >> id;
+        int time = hour * 60 + min;
+        return new Leave(numberOfStations, id, time);
     }
 
     void GetPassengersOff(int time, string Filename)
@@ -95,21 +100,21 @@ private:
         {
             Station Station = StationsArray[i];
             // Loop through NormalPassengersMovingBusesForward
-            
+
             while (!Station.getNormalPassengersMovingBusesForward().isEmpty())
             {
                 Bus *bus = Station.getNormalPassengersMovingBusesForward().dequeue();
                 Bus Bus = *bus;
-                Bus.checkEndStationAndRemove(Station, Filename, time, FinishList,BoardingTime);
+                Bus.checkEndStationAndRemove(Station, Filename, time, FinishList, BoardingTime);
             }
 
             // Loop through NormalPassengersMovingBusesBackward
-           
+
             while (!Station.getNormalPassengersMovingBusesBackward().isEmpty())
             {
                 Bus *bus = Station.getNormalPassengersMovingBusesBackward().dequeue();
                 Bus Bus = *bus;
-                Bus.checkEndStationAndRemove(Station, Filename, time, FinishList,BoardingTime);
+                Bus.checkEndStationAndRemove(Station, Filename, time, FinishList, BoardingTime);
             }
 
             // Loop through WheelchairPassengersMovingBusesForward
@@ -118,16 +123,16 @@ private:
             {
                 Bus *bus = Station.getWheelchairPassengersMovingBusesForward().dequeue();
                 Bus Bus = *bus;
-                Bus.checkEndStationAndRemove(Station, Filename, time, FinishList,BoardingTime);
+                Bus.checkEndStationAndRemove(Station, Filename, time, FinishList, BoardingTime);
             }
 
             // Loop through WheelchairPassengersMovingBusesBackward
-          
+
             while (!Station.getWheelchairPassengersMovingBusesBackward().isEmpty())
             {
                 Bus *bus = Station.getWheelchairPassengersMovingBusesBackward().dequeue();
                 Bus Bus = *bus;
-                Bus.checkEndStationAndRemove(Station, Filename, time, FinishList,BoardingTime);
+                Bus.checkEndStationAndRemove(Station, Filename, time, FinishList, BoardingTime);
             }
         }
     }
@@ -138,39 +143,39 @@ private:
         {
             Station Station = StationsArray[i];
             // Loop through NormalPassengersMovingBusesForward
-            
+
             while (!Station.getNormalPassengersMovingBusesForward().isEmpty())
             {
                 Bus *bus = Station.getNormalPassengersMovingBusesForward().dequeue();
                 Bus Bus = *bus;
-                Bus.boardPassengers(Station, Filename, time,MixedBusCapacity,WheelBusCapacity,BoardingTime);
+                Bus.boardPassengers(Station, Filename, time, MixedBusCapacity, WheelBusCapacity, BoardingTime);
             }
 
             // Loop through NormalPassengersMovingBusesBackward
-            
+
             while (!Station.getNormalPassengersMovingBusesBackward().isEmpty())
             {
-                Bus *bus =  Station.getNormalPassengersMovingBusesBackward().dequeue();
+                Bus *bus = Station.getNormalPassengersMovingBusesBackward().dequeue();
                 Bus Bus = *bus;
-                Bus.boardPassengers(Station, Filename, time,MixedBusCapacity,WheelBusCapacity,BoardingTime);
+                Bus.boardPassengers(Station, Filename, time, MixedBusCapacity, WheelBusCapacity, BoardingTime);
             }
 
             // Loop through WheelchairPassengersMovingBusesForward
-           
+
             while (!Station.getWheelchairPassengersMovingBusesForward().isEmpty())
             {
                 Bus *bus = Station.getWheelchairPassengersMovingBusesForward().dequeue();
                 Bus Bus = *bus;
-                Bus.boardPassengers(Station, Filename, time,MixedBusCapacity,WheelBusCapacity,BoardingTime);
+                Bus.boardPassengers(Station, Filename, time, MixedBusCapacity, WheelBusCapacity, BoardingTime);
             }
 
             // Loop through WheelchairPassengersMovingBusesBackward
-            
+
             while (!Station.getWheelchairPassengersMovingBusesBackward().isEmpty())
             {
                 Bus *bus = Station.getWheelchairPassengersMovingBusesBackward().dequeue();
                 Bus Bus = *bus;
-                Bus.boardPassengers(Station, Filename, time,MixedBusCapacity,WheelBusCapacity,BoardingTime);
+                Bus.boardPassengers(Station, Filename, time, MixedBusCapacity, WheelBusCapacity, BoardingTime);
             }
         }
     };
@@ -274,7 +279,7 @@ private:
         pntr = &psngr;
         FinishList.enqueue(pntr);
     }
-        void Output()
+    void Output()
     {
         ofstream outFile("output.txt");
         outFile << "FT\t\t\t\tID\t\t\t\tAT\t\t\t\tWT\t\t\t\tTT" << endl; // FT -> Finish Time  AT -> Arrival Time  WT -> Waiting Time  TT -> Trip Time
@@ -332,14 +337,12 @@ private:
         outFile << "Avg Utilization : " << totalUtilize / totalBuses * 100 << "%\n";
     }
 
-    
-
 public:
     LinkedQueue<Passenger *> &getFinishlist()
     {
         return FinishList;
     }
-    
+
     void ReadFile(string fileName)
     {
         // Read Number Of Station
@@ -356,88 +359,87 @@ public:
         MixedBusCount = *(ConvertToInt(GetFileLine(fileName, 2, 'O')) + 1);
         // Read Number Of Mixed Busses
         WheelBusCount = *(ConvertToInt(GetFileLine(fileName, 2, 'O')));
-        //Read Capacity of Mixed Bus
-        MixedBusCapacity=*(ConvertToInt(GetFileLine(fileName, 3, 'O')));
-        //Read Capacity of Wheel Bus
-        WheelBusCapacity=*(ConvertToInt(GetFileLine(fileName, 3, 'O'))+1);
+        // Read Capacity of Mixed Bus
+        MixedBusCapacity = *(ConvertToInt(GetFileLine(fileName, 3, 'O')));
+        // Read Capacity of Wheel Bus
+        WheelBusCapacity = *(ConvertToInt(GetFileLine(fileName, 3, 'O')) + 1);
         // Read Number of jornyes needed before Checkup
         JourneysToCheckup = *(ConvertToInt(GetFileLine(fileName, 4, 'O')));
         // Read duration of checkup for Wbus
-        checkup_durations_Wb=*(ConvertToInt(GetFileLine(fileName, 4, 'O'))+1);
+        checkup_durations_Wb = *(ConvertToInt(GetFileLine(fileName, 4, 'O')) + 1);
         // Read duration of checkup for Mbus
-        checkup_durations_Mb=*(ConvertToInt(GetFileLine(fileName, 4, 'O'))+2);
-        EnqueueGarage(WheelBusCount,MixedBusCount,JourneysToCheckup);
+        checkup_durations_Mb = *(ConvertToInt(GetFileLine(fileName, 4, 'O')) + 2);
+        EnqueueGarage(WheelBusCount, MixedBusCount, JourneysToCheckup);
         // Read MAx Waiting Time
-        MaxWaitingTime=*(ConvertToInt(GetFileLine(fileName, 5, 'O')));
+        MaxWaitingTime = *(ConvertToInt(GetFileLine(fileName, 5, 'O')));
         // Read Boarding Time
-        BoardingTime=*(ConvertToInt(GetFileLine(fileName, 5, 'O'))+1);
+        BoardingTime = *(ConvertToInt(GetFileLine(fileName, 5, 'O')) + 1);
         // Read Number OF Events
-        NumberOfEvents=*(ConvertToInt(GetFileLine(fileName,6, 'O')));
+        NumberOfEvents = *(ConvertToInt(GetFileLine(fileName, 6, 'O')));
         // Read File Events
-        GetFileLine(fileName,7,'E');
+        GetFileLine(fileName, 7, 'E');
     }
 
-
-    string* GetFileLine(string& fileName,int lineNum,char Functionality/*E to read Events, O to read only this line*/)
+    string *GetFileLine(string &fileName, int lineNum, char Functionality /*E to read Events, O to read only this line*/)
     {
-    // Use the ifstream object file to read the file
-    ifstream file;
-    // filename will store the name of the file and line_number the line number
-    string filename=fileName;
-    int Capacity=*(CalcCapacity(filename));
-    string* FileArray = new string[Capacity] ;//to be changed to linked list
-    int line_number=lineNum;
-    
+        // Use the ifstream object file to read the file
+        ifstream file;
+        // filename will store the name of the file and line_number the line number
+        string filename = fileName;
+        int Capacity = *(CalcCapacity(filename));
+        string *FileArray = new string[Capacity]; // to be changed to linked list
+        int line_number = lineNum;
 
-    // Open the file with the provided filename
-    file.open(filename);
+        // Open the file with the provided filename
+        file.open(filename);
 
-    // If there was a failure opening the file (perhaps it does not exist) then
-    // exit with an error message and status.
-    if (file.fail())
-    {
-        cout << "File failed to open." << endl;
-        return 0;
-    }
-
-
-    int current_line = 0;
-    string line;
-
-    // Continue to read the file one line at a time, unless we reach the end of 
-    while (!file.eof())
-    {
-    // Increment the current line number being read as we are reading in the 
-        current_line++;
-
-    // Read the next line from the 'file' into the 'line' string 
-    getline(file, line);
-
-    if (Functionality=='O'){
-        // If current line number of the line we've read in matches the line number 
-        // that we're looking for, use break to stop the loop
-        if (current_line == line_number)
-           FileArray=SplitString(line);//to be fixed when linked list
-            break;
+        // If there was a failure opening the file (perhaps it does not exist) then
+        // exit with an error message and status.
+        if (file.fail())
+        {
+            cout << "File failed to open." << endl;
+            return 0;
         }
-    if (Functionality=='E'){
-        while(NumberOfEvents<=0){
-            char EventType;
-            file>>EventType;
-            Events * Event = EventType == 'A' ? CreateArrive(file) : CreateLeave(file);
-            EventsList.enqueue(Event);
-        }
-    }
-    }
-    file.close();
-    return FileArray;
 
+        int current_line = 0;
+        string line;
+
+        // Continue to read the file one line at a time, unless we reach the end of
+        while (!file.eof())
+        {
+            // Increment the current line number being read as we are reading in the
+            current_line++;
+
+            // Read the next line from the 'file' into the 'line' string
+            getline(file, line);
+
+            if (Functionality == 'O')
+            {
+                // If current line number of the line we've read in matches the line number
+                // that we're looking for, use break to stop the loop
+                if (current_line == line_number)
+                    FileArray = SplitString(line); // to be fixed when linked list
+                break;
+            }
+            if (Functionality == 'E')
+            {
+                while (NumberOfEvents <= 0)
+                {
+                    char EventType;
+                    file >> EventType;
+                    Events *Event = EventType == 'A' ? CreateArrive(file) : CreateLeave(file);
+                    EventsList.enqueue(Event);
+                }
+            }
+        }
+        file.close();
+        return FileArray;
     }
 
     void Simulate(const string &fileName)
     {
         ui.Mode();
-        int time = 240; // Equivelent to 4 oclock
+        int time = 240;     // Equivelent to 4 oclock
         while (time < 1320) // loop while time is between 4 am oclock and 10 pm oclock
         {
             MoveBusFromGarage(time); //  bus from station #0 to movingbusses list
@@ -460,6 +462,41 @@ public:
         }
     }
 
+    void addToMovingListNP(int time)
+    {
+        for (int i = 0; i < numberOfStations; i++)
+        {
+            Bus *bus = StationsArray[i].removeNPFwdBus();
+            if (bus)
+            {
+                bus->setMovingTime(time);
+                Moving_Busses.enqueue(bus);
+            }
+            bus = StationsArray[i].removeNPBwdBus();
+            if (bus)
+            {
+                bus->setMovingTime(time);
+                Moving_Busses.enqueue(bus);
+            }
+        }
+    }
 
-    
+    void addToMovingListWCP(int time)
+    {
+        for (int i = 0; i < numberOfStations; i++)
+        {
+            Bus *bus = StationsArray[i].removeWCPFwdBus();
+            if (bus)
+            {
+                bus->setMovingTime(time);
+                Moving_Busses.enqueue(bus);
+            }
+            bus = StationsArray[i].removeWCPBwdBus();
+            if (bus)
+            {
+                bus->setMovingTime(time);
+                Moving_Busses.enqueue(bus);
+            }
+        }
+    }
 };
